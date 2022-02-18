d00315c442c9c1258d0fefc7c7cebdd6f2c367c3
ys0222 <91381230+ys0222@users.noreply.github.com>
2022-02-18 21:47:18 +0900

    Update 0218.ts

--- 22-02/0218.ts
+++ 22-02/0218.ts
@@ -1,6 +1,6 @@
-#함수 선언문
+#함수 선언문 #화살표 함수와 표현식 문(105)
 
-//function 키워드로 만드는 함수
+◆function 키워드로 만드는 함수◆
 
 function 함수이름(매개변수1 :타입1 , 매개변수2 : 타입2 [, ...]) : 반환값 타입{
     함수 내용
@@ -22,7 +22,7 @@ console.log(result)
 
 여기서 a,b 는 parameter 매개변수라 부르고 add(1,2)에서 1과 2는 argument 인수라 부른다.
 
-//void 타입
+◆void 타입◆
 
 function printMe(name : string , age : number) : void {
     console.log(`name : ${name}, age : ${age}`)
@@ -30,7 +30,7 @@ function printMe(name : string , age : number) : void {
 
 값을 반환하지 않는 함수는 반환 타입이 void 입니다. void 타입은 함수 반환 타입으로만 사용 할 수 있다.
 
-//함수 시그니처
+◆함수 시그니처◆
 
 (매개변수1 타입, 매개변수2 타입 [, ...]) => 반환값 타입
 
@@ -38,7 +38,7 @@ printMe라는 함수의 시그니처를 이용한 예시이다.
 
 let printMe: (string, number) => void 
 
-// type 키워드
+◆type 키워드◆
 
 type 새로운 타입 = 기존 타입
 
@@ -51,7 +51,7 @@ type키워드는 기존에 존재하는 타입을 이름만 바꿔 사용할 수
 01행에서 stringNumberFunc이라는 이름으로 타입별칭을 만든다.
 그러면 02행과 03행에서 변수 f와g에 타입 주석을 더 수월하게 만들 수 있다.
 
-//선택적 매개변수
+◆선택적 매개변수◆
 
 function fn(arg1: string , arg ?: number) 
 {
@@ -67,7 +67,7 @@ type OptionalArgFnc = (string, number?) => void
 
 함수의 시그니처에서는 타입 뒤에 물음표를 적습니다.
 
-//일등함수
+◆일등함수◆
 
 일등함수는 함수와 변수를 구분하지 않는다는 의미입니다.
 
@@ -76,7 +76,7 @@ f = function(a,b) {return a-b}
 
 f는 변수입니다. 그래서 a+b식도 저장되고 a-b식으로 바꿔서 저장할 수도 있습니다.
 
-//함수 호출 연산자
+◆함수 호출 연산자◆
 
 let fc = function (a,b) {return a+b}
 let result = fc(1,2)
@@ -84,7 +84,7 @@ let result = fc(1,2)
 어떤 변수가 함수표현식을 담고 있으면 호출연산자()를 붙여 호출할 수 있다.
 매개변수를 요구하면 매개변수를 ()안에 적어서 호출한다.
 
-//익명 함수
+◆익명 함수◆
 
 let value = (function (a,b) {return a+b;})(1,2)
 
@@ -93,7 +93,7 @@ let value = (function (a,b) {return a+b;})(1,2)
 익명함수의 작동원리는 연산자가 우선순위에 따라 계산해준다.
 (function (a,b) {return a+b;})가 먼저 실행되고 (1,2)을 만나 vlaue라는 변수에 값을 대입합니다.
 
-//const 키워드와 함수 표현식
+◆const 키워드와 함수 표현식◆
 
 let f = () => {} 
 const f = () = > {}
@@ -101,3 +101,35 @@ const f = () = > {}
 let은 변수 값이 바뀔 수 있다. 즉 함수내용이 바뀔 수 있다.
 반면, const는 변수의 값이 바뀌지않는다. 즉 함수의 내용도 바뀌지않는다.
 그래서 함수 표현식을 담는 변수에는 const를 사용하는것이 바람직하다고 한다.
+
+#화살표 함수와 표현식 문
+
+◆화살표 함수◆
+
+const 함수이름 = (매개변수1 : 타입1 , 매개변수2 : 타입2 [, ...]) : 반환 타입 => 함수몸통
+
+const plus = (a : number , b : number) : number => {return a+b} //실행문 방식 몸통
+const plus = (a : number , b : number) : number => return a+b // 표현식문 방식 몸통
+
+그런데 화살표 함수에서는 함수몸통에 {}을 생략할 수 도 있습니다.
+
+◆실행문 방식과 표현식문 방식의 차이점◆
+
+실행문 방식 : return 을 적용해야 결과를 알 수 있음 ex) C
+표현식문 방식 : return 을 적용하지 않아도 결과를 알 수 있음 ex) JavaScript
+실행문 방식과 표현식문 방식이 결합된 언어는 다중 패러다임 언어라고한다.
+typescripte는 다중 패러다임 언어라고 한다.
+
+◆복합 실행문◆
+
+if(조건식)
+    실행문
+    
+if와 같은 구문은 다음처럼 조건을 만족하면 단순히 하눚ㄹ의 실행문만을 실행하는 형태로 설계된다.
+이런 설계가 가능한 이유는 복학 실행문 이라는 또 다른 형태를 함께 제공하기 때문이다. {}을 이용한다.
+
+if(조건식) {
+    실행문
+}
+
+복합 실행문은 컴파일러가 여러 실행문을 한개로 인식한다. 

047a9a142e6c3b6d39d3c0949da8a7f8c45227c8
ys0222 <91381230+ys0222@users.noreply.github.com>
2022-02-18 17:55:45 +0900

    Update 0218.ts

--- 22-02/0218.ts
+++ 22-02/0218.ts
@@ -66,3 +66,38 @@ fn('hello')
 type OptionalArgFnc = (string, number?) => void
 
 함수의 시그니처에서는 타입 뒤에 물음표를 적습니다.
+
+//일등함수
+
+일등함수는 함수와 변수를 구분하지 않는다는 의미입니다.
+
+let f = function(a,b) {return a+b}
+f = function(a,b) {return a-b}
+
+f는 변수입니다. 그래서 a+b식도 저장되고 a-b식으로 바꿔서 저장할 수도 있습니다.
+
+//함수 호출 연산자
+
+let fc = function (a,b) {return a+b}
+let result = fc(1,2)
+
+어떤 변수가 함수표현식을 담고 있으면 호출연산자()를 붙여 호출할 수 있다.
+매개변수를 요구하면 매개변수를 ()안에 적어서 호출한다.
+
+//익명 함수
+
+let value = (function (a,b) {return a+b;})(1,2)
+
+함수 표현식 개념이 없는 함수가 익명 함수이다.
+
+익명함수의 작동원리는 연산자가 우선순위에 따라 계산해준다.
+(function (a,b) {return a+b;})가 먼저 실행되고 (1,2)을 만나 vlaue라는 변수에 값을 대입합니다.
+
+//const 키워드와 함수 표현식
+
+let f = () => {} 
+const f = () = > {}
+
+let은 변수 값이 바뀔 수 있다. 즉 함수내용이 바뀔 수 있다.
+반면, const는 변수의 값이 바뀌지않는다. 즉 함수의 내용도 바뀌지않는다.
+그래서 함수 표현식을 담는 변수에는 const를 사용하는것이 바람직하다고 한다.
